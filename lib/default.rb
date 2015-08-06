d261394c15860e5df09ca2433b86acee98148d4d
rejuvyesh <mail@rejuvyesh.com>
2015-08-07 03:28:52 +0530

    move to their own partials

--- lib/default.rb
+++ lib/default.rb
@@ -6,6 +6,8 @@
 # Copyright rejuvyesh <mail@rejuvyesh.com>, 2014
 # License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
 
+include Nanoc::Helpers::Rendering
+
 def route_with_new_extension ext
   item.identifier.without_ext + '.' + ext
 end
