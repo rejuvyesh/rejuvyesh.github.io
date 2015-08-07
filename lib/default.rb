75e28e21ac1f254e557ad615ca66edf6c2cc4c9f
rejuvyesh <mail@rejuvyesh.com>
2015-08-07 08:20:32 +0530

    blogging helpers

--- lib/default.rb
+++ lib/default.rb
@@ -7,6 +7,10 @@
 # License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
 
 include Nanoc::Helpers::Rendering
+include Nanoc::Helpers::Text
+include Nanoc::Helpers::Blogging
+include Nanoc::Helpers::Tagging
+include Nanoc::Helpers::LinkTo
 
 def route_with_new_extension ext
   item.identifier.without_ext + '.' + ext
